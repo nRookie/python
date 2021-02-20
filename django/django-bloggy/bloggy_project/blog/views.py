479b7ac818c96b3b128e3c5853377cf2b86be81b
nRookie <228611676@qq.com>
2021-02-20 14:25:38 +0800

    feat(django): make the url more readable.

--- django/django-bloggy/bloggy_project/blog/views.py
+++ django/django-bloggy/bloggy_project/blog/views.py
@@ -10,12 +10,15 @@ from blog.models import Post
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
     t = loader.get_template('blog/index.html')
-    # c = Context({'latest_posts': latest_posts, })
-    c = {'latest_posts': latest_posts, }
-    return HttpResponse(t.render(c))
+    context_dict = {'latest_posts': latest_posts, }
+    for post in latest_posts:
+        post.url = post.title.replace(' ', '_')
 
-def post(request, post_id):
-    single_post = get_object_or_404(Post, pk=post_id)
+    # c = {'latest_posts': latest_posts, }
+    return HttpResponse(t.render(context_dict))
+
+def post(request, post_url):
+    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
     t = loader.get_template('blog/post.html')
     # c = Context({'single_post' : single_post, })
     c = {'single_post' : single_post, }
