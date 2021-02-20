3e822feb97bb1a28d864d8d02c55c49f3c32942e
nRookie <228611676@qq.com>
2021-02-20 13:47:57 +0800

    feat(django): add tables into admins. and create a user nRookie

--- django/django-bloggy/bloggy_project/blog/admin.py
+++ django/django-bloggy/bloggy_project/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+
+admin.site.register(Post, PostAdmin)
