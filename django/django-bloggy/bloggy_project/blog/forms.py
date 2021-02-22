ba3af63e802c031c811f7acec5cd4cef448a2f7f
nRookie <228611676@qq.com>
2021-02-22 10:40:17 +0800

    feat(django): remove the form field.

--- django/django-bloggy/bloggy_project/blog/forms.py
+++ django/django-bloggy/bloggy_project/blog/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tag', 'image', 'views']
+        fields = ['title', 'content', 'tag', 'image']
 
 
 ''' Here,
