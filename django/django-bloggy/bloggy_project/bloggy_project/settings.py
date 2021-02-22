46bc7a2f4c59226c635bc58f22efa4fff1f2b19a
nRookie <228611676@qq.com>
2021-02-22 10:39:18 +0800

    feat(django): modify templates through bootstrap

--- django/django-bloggy/bloggy_project/bloggy_project/settings.py
+++ django/django-bloggy/bloggy_project/bloggy_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    # Local Apps
+
+    # Third Party Apps
+    'django_forms_bootstrap',
+
+     # Local Apps
     'blog',
 ]
 
