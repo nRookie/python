e529a9abf039c05cef1ad80fb950eee7dce2d160
nRookie <228611676@qq.com>
2021-02-25 09:58:07 +0800

    tests (django-ecommerce): add a simple route test case

--- django/django_ecommerce/django_ecommerce/main/tests.py
+++ django/django_ecommerce/django_ecommerce/main/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import index
+
+class MainPageTests(TestCase):
+    
+    def test_root_resolves_to_main_view(self):
+        main_page = resolve('/')
+        self.assertEqual(main_page.func, index)
 
 # Create your tests here.
