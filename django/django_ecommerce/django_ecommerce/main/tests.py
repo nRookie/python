57ba22f560645612604da8c934a10907a9b6a4d8
nRookie <228611676@qq.com>
2021-02-26 11:54:05 +0800

    feat(django-commerce): add render test case to make it work, maybe inappropriate

--- django/django_ecommerce/django_ecommerce/main/tests.py
+++ django/django_ecommerce/django_ecommerce/main/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import resolve
 from .views import index
-# from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
+from django.test import RequestFactory
+
 class MainPageTests(TestCase):
     
     def test_root_resolves_to_main_view(self):
@@ -20,7 +22,31 @@ class MainPageTests(TestCase):
     # def test_returns_exact_html(self):
     #     index = self.client.get("/")
     #     self.assertEquals(
-    #     index.content,
-    #     render_to_response("index.html").content
+    #         index.content,
+    #         render_to_string('index.html')
     #     )
-# Create your tests here.
+    
+    def test_index_handles_logged_in_user(self):
+        # create the user needed for user lookup from index page
+        from payments.models import User
+        user = User(
+            name='jj',
+            email='j@j.com'
+        )
+        user.save()
+        
+        # create a Mock request object
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.session = {"user": "1"} # make sure it has an associated session
+        
+        #request the index page
+        resp = index(request)
+        
+        # verify it returns the page for the logged in user.
+        
+        self.assertEquals (
+            resp.content.decode().replace(' ', ''),
+            render_to_string('user.html', {'user': user}).replace(' ', '')
+        )
+        # self.assertTemplateUsed(resp, 'user.html')
