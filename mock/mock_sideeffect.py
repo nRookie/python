dead43a67389f0f75ce0bd121bbc6448c8f59405
nRookie <228611676@qq.com>
2021-04-27 16:05:41 +0800

    feat(python): add mock related objects.

---
+++ mock/mock_sideeffect.py
@@ -0,0 +1,24 @@
+import unittest
+import requests
+from requests.exceptions import Timeout
+from unittest.mock import Mock
+
+def get_holidays():
+    r = requests.get('http://localhost/api/holidays')
+    if r.status_code == 200:
+        return r.json()
+    return None
+
+
+requests = Mock()
+
+class TestCalendar(unittest.TestCase):
+    def test_get_holidays_timeout(self):
+        # Test a connection timeout
+        requests.get.side_effect = Timeout
+        with self.assertRaises(Timeout):
+            get_holidays()
+
+
+if __name__ == '__main__':
+    unittest.main()
